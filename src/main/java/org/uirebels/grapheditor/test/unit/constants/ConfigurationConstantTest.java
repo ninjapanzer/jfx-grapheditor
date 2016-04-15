a12af88a371b2c210a3e0784446cee8a93761ada
Paul Scarrone <paul.scarrone@gmail.com>
2016-04-15 19:34:11 -0400

    WIP sample tests and configuration

---
+++ src/main/java/org/uirebels/grapheditor/test/unit/constants/ConfigurationConstantTest.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.*;
+import constants.ConfigurationContstant;
+
+public class ConfigurationContstantTest {
+
+ @BeforeClass
+ public void setUp() {
+   // code that will be invoked when this test is instantiated
+ }
+
+ @Test(groups = { "fast" })
+ public void aFastTest() {
+   System.out.println("Fast test");
+ }
+
+ @Test(groups = { "slow" })
+ public void aSlowTest() {
+    System.out.println("Slow test");
+ }
+
+}
